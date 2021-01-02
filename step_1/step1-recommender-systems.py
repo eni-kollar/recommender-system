4c159add2854b8f75148027bb64023f4b4234a3f
bkolar <b.t.a.kolar@student.tudelft.nl>
2021-01-02 19:56:00 +0100

    working step 3 of collab filtering

--- step_1/step1-recommender-systems.py
+++ step_1/step1-recommender-systems.py
@@ -35,9 +35,9 @@ ratings_description = pd.read_csv(ratings_file, delimiter=';',
 predictions_description = pd.read_csv(predictions_file, delimiter=';', names=['userID', 'movieID'], header=None)
 
 def predict_test(movies, users, ratings, predictions):
-    user_movie_matrix = populate_user_movie_matrix(movies, users, ratings)
-    user_sim_matrix = create_user_similarity_matrix(users, user_movie_matrix)
-    print(user_sim_matrix)
+    user_movie_matrix = populate_user_movie_matrix(movies, ratings)
+    user_sim_matrix = create_user_similarity_matrix(user_movie_matrix)
+    print(get_neighbours(user_movie_matrix, user_sim_matrix, 1, 1, 17))
 
 
 #####
@@ -46,15 +46,9 @@ def predict_test(movies, users, ratings, predictions):
 ##
 #####
 
-def populate_user_movie_matrix(movies, users, ratings):
-    user_movie_matrix = np.empty((users.size + 1, movies.size + 1))
-    #print(user_movie_matrix)
-    for i in range(0, len(ratings)):
-        user_movie_matrix[ratings.iloc[i]['userID'], ratings.iloc[i]['movieID']] = ratings.iloc[i]['rating']
-        # print(ratings.iloc[i]['userID'], ratings.iloc[i]['movieID'], ratings.iloc[i]['rating'])
-        # print(ratings.iloc[i]['userID'])
-    # print(user_movie_matrix)
-
+def populate_user_movie_matrix(movies, ratings):
+    movie_data = pd.merge(ratings, movies, on='movieID')
+    user_movie_matrix = movie_data.pivot_table(index='movieID', columns='userID', values='rating')
     return user_movie_matrix
 
 #####
@@ -63,29 +57,64 @@ def populate_user_movie_matrix(movies, users, ratings):
 ##
 #####
 
-def create_user_similarity_matrix(users, user_movie_matrix):
-    user_sim_matrix = np.empty((users.size + 1, users.size + 1))
-    for i in range(0, len(users)):
-        for j in range(i, len(users)):
-            if i == 0 or j == 0:
-                user_sim_matrix[i][j] = 0
-            else:
-                user_1 = users.iloc[i]['userID']
-                user_2 = users.iloc[j]['userID']
-                ratings_u1 = pd.Series(user_movie_matrix[i])
-                ratings_u2 = pd.Series(user_movie_matrix[j])
-                user_sim_matrix[i][j] = similarity(ratings_u1, ratings_u2)
-                user_sim_matrix[j][i] = similarity(ratings_u1, ratings_u2)
-    return user_sim_matrix
-
-def similarity(list_1, list_2):
-    return list_1.corr(list_2)
+def create_user_similarity_matrix(user_movie_matrix):
+    user_similarity_matrix = user_movie_matrix.replace(np.nan, 0)
+    user_similarity_matrix = np.corrcoef(user_similarity_matrix.values.T)
+    user_similarity_matrix = pd.DataFrame(data=user_similarity_matrix[0:, 0:], index=user_movie_matrix.columns, columns=user_movie_matrix.columns)
+    return user_similarity_matrix
 
 #####
 ##
 ## COLLABORATIVE FILTERING
 ##
 #####
+
+
+# get N users that are most similar to user A that has also rated this movie
+
+def get_n_nearest_neighbour(user_user_similarity_matrix, n, user_id):
+    # -1 to get the indexing right
+    all_neighbours = user_user_similarity_matrix.iloc[[user_id - 1]]
+    # convert pandas dataframe to numpy ndarray. Get first column of array
+    # (it only has one anyway, because we are only taking a single row)
+    # to make it a 1D array from a 2D array.
+    all_neighbours_np_array = all_neighbours.to_numpy()[0]
+    # get index of the top n items in the array
+    closest_n_neighbours_index = all_neighbours_np_array.argsort()[::-1][:n]
+    # add 1 to the indecies to get the corresponding user_id
+    # (because indecies start from 0, while user_ids start from 1)
+    closest_n_neighbours = [i + 1 for i in closest_n_neighbours_index]
+    # return list of n most similar user_ids
+    return closest_n_neighbours
+
+# Select top N dataset by similarity threshold
+def get_neighbours(user_mov_matrix, similarity_matrix, user_id, movie_id, n):
+    # user_sim_column = similarity_matrix.loc[:,user_id]
+    #
+    # #returns array of similar users
+    # find_neighbors = [user_sim_column.index[i] for i, v in enumerate(user_sim_column) if v>=0.3]
+    # # print(similarity_matrix.loc[1, 96])
+    # # print(find_neighbors)
+
+    nearest_n = get_n_nearest_neighbour(similarity_matrix, n, 1)
+    neighbor_ratings = user_mov_matrix.loc[movie_id][nearest_n].fillna(0)
+    print(neighbor_ratings)
+    neighbor_similarity = similarity_matrix[user_id].loc[nearest_n]
+    print(neighbor_similarity)
+    score = np.dot(neighbor_similarity, neighbor_ratings) / neighbor_similarity.sum()
+    return score.round()
+    # return calc_rating(neighbor_ratings, neighbor_similarity, user_mov_matrix, user_id)
+
+# def find_neighbors(user_sim_column):
+#     return [user_sim_column.index[i] for i, v in enumerate(user_sim_column) if v>=0.3]
+
+# def calc_rating(neighbor_ratings, neighbor_similarity, user_movie_matrix, user_id):
+#     # mean_rating = np.mean(user_movie_matrix.loc[:, user_id])
+#     # score = np.dot(neighbor_similarity, neighbor_ratings) + mean_rating
+#     score = np.dot(neighbor_similarity, neighbor_ratings) / neighbor_similarity.sum()
+#     # data = score.reshape(1, len(score))
+#     # columns = neighbor_ratings.columns
+#     return score
 
 def predict_collaborative_filtering(movies, users, ratings, predictions):
     # Create user movie matrix containing ratings
