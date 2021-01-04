c84e2f3e2be6ea346685278806ecb966007de2ba
bkolar <b.t.a.kolar@student.tudelft.nl>
2021-01-04 08:24:01 +0100

    Refactored

--- step_1/step1-recommender-systems.py
+++ step_1/step1-recommender-systems.py
@@ -36,34 +36,7 @@ ratings_description = pd.read_csv(ratings_file, delimiter=';',
 predictions_description = pd.read_csv(predictions_file, delimiter=';', names=['userID', 'movieID'], header=None)
 
 def predict_test(movies, users, ratings, predictions):
-    start_t = time.time()
-    user_movie_matrix = populate_user_movie_matrix(movies, ratings)
-    print('creating user_movie matrix took:', time.time() - start_t)
-
-    user_movie_matrix, user_mean_rating = normalization(user_movie_matrix)
-    print('normalizing user_movie matrix took:', time.time() - start_t)
-
-    user_sim_matrix = create_user_similarity_matrix(user_movie_matrix)
-    print('creating user_user similarity matrix took:', time.time() - start_t)
-    print('creating matrices took:', time.time() - start_t)
-
-    # predictions_short = predictions.head(10000)
-    # print(predictions_short)
-
-    predicted_ratings = predictions.apply(lambda row: get_neighbours(user_movie_matrix, user_sim_matrix, row['userID'],
-                                 row['movieID'], user_mean_rating, 5), axis=1)
-    print('making predictions took:', time.time() - start_t)
-
-    result_ratings = pd.Series(predicted_ratings).to_numpy()
-    print('num of generated scores:', len(result_ratings))
-    print('num of predictions read:', len(predictions))
-    # result_ids = pd.Series(range(1, len(predicted_ratings) + 1)).to_numpy()
-    ratings_final = []
-    for i in range(0, len(predictions)):
-        ratings_final.append((i+1, result_ratings[i]))
-    print('length of ratings final:', len(ratings_final))
-    print('submitting predictions:', time.time() - start_t)
-    return ratings_final
+    return predict_latent_factors(movies, users, ratings, predictions)
 
 
 def normalization(matrix):
@@ -135,9 +108,34 @@ def get_neighbours(user_mov_matrix, similarity_matrix, user_id, movie_id, user_m
     return round(score, 4)
 
 def predict_collaborative_filtering(movies, users, ratings, predictions):
-    # Create user movie matrix containing ratings
+    start_t = time.time()
+    user_movie_matrix = populate_user_movie_matrix(movies, ratings)
+    print('creating user_movie matrix took:', time.time() - start_t)
 
-    pass
+    user_movie_matrix, user_mean_rating = normalization(user_movie_matrix)
+    print('normalizing user_movie matrix took:', time.time() - start_t)
+
+    user_sim_matrix = create_user_similarity_matrix(user_movie_matrix)
+    print('creating user_user similarity matrix took:', time.time() - start_t)
+    print('creating matrices took:', time.time() - start_t)
+
+    # predictions_short = predictions.head(10000)
+    # print(predictions_short)
+
+    predicted_ratings = predictions.apply(lambda row: get_neighbours(user_movie_matrix, user_sim_matrix, row['userID'],
+                                                                     row['movieID'], user_mean_rating, 5), axis=1)
+    print('making predictions took:', time.time() - start_t)
+
+    result_ratings = pd.Series(predicted_ratings).to_numpy()
+    print('num of generated scores:', len(result_ratings))
+    print('num of predictions read:', len(predictions))
+    # result_ids = pd.Series(range(1, len(predicted_ratings) + 1)).to_numpy()
+    ratings_final = []
+    for i in range(0, len(predictions)):
+        ratings_final.append((i + 1, result_ratings[i]))
+    print('length of ratings final:', len(ratings_final))
+    print('submitting predictions:', time.time() - start_t)
+    return ratings_final
 
 
 #####
@@ -147,9 +145,11 @@ def predict_collaborative_filtering(movies, users, ratings, predictions):
 #####
 
 def predict_latent_factors(movies, users, ratings, predictions):
-    ## TO COMPLETE
+    user_movie_matrix = populate_user_movie_matrix(movies, ratings)
+    user_movie_matrix_normalized, user_mean_rating = normalization(user_movie_matrix)
 
-    pass
+    return
+
 
 
 #####
